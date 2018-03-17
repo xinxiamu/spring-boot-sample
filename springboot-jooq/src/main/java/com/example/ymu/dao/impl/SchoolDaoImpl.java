d41b7c5339bd5edaadb3f8ad1a7ef610141a4349
xinxiamu <932852117@qq.com>
2018-03-17 10:37:30 +0800

    更改

--- springboot-jooq/src/main/java/com/example/ymu/dao/impl/SchoolDaoImpl.java
+++ springboot-jooq/src/main/java/com/example/ymu/dao/impl/SchoolDaoImpl.java
@@ -7,16 +7,16 @@ import org.springframework.stereotype.Repository;
 import com.example.ymu.dao.SchoolDao;
 import com.example.ymu.dao.base.BaseDaoImpl;
 import com.example.ymu.dao.repository.SchoolRepository;
-import static com.example.demo.jooq.tables.School.*;
+//import static com.example.demo.jooq.tables.School.*;
 
 @Repository
 public class SchoolDaoImpl extends BaseDaoImpl<SchoolRepository> implements SchoolDao {
 
 	@Override
 	public void findSchoolName() {
-		
-		Result<Record> a = jooq.select().from(SCHOOL).fetch();
-		System.out.println("---sql:" + a.size()); 
+//
+//		Result<Record> a = jooq.select().from(SCHOOL).fetch();
+//		System.out.println("---sql:" + a.size());
 	}
 
 }
