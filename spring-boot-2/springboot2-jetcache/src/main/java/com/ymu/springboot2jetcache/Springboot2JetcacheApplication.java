82a695ebed8a8eb125dd8d6c3c2642f127a0fda8
xinxiamu <932852117@qq.com>
2018-05-25 15:38:55 +0800

    jetcache demo

---
+++ spring-boot-2/springboot2-jetcache/src/main/java/com/ymu/springboot2jetcache/Springboot2JetcacheApplication.java
@@ -0,0 +1,12 @@
+package com.ymu.springboot2jetcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2JetcacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2JetcacheApplication.class, args);
+    }
+}
