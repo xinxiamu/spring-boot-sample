8db572eb118c3e1307365ed266c65bf1263bb539
xinxiamu <932852117@qq.com>
2018-08-06 21:40:14 +0800

    修改

---
+++ spring-boot-2/springboot2-webservice-cxf/src/main/java/com/example/springboot2webservicecxf/Springboot2WebserviceCxfApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot2webservicecxf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2WebserviceCxfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2WebserviceCxfApplication.class, args);
+    }
+}
---
+++ spring-boot-2/springboot2-webservice-cxf/src/test/java/com/example/springboot2webservicecxf/Springboot2WebserviceCxfApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springboot2webservicecxf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot2WebserviceCxfApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
