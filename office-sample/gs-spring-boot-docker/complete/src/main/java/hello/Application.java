72f9a5f34c8b7e07b7f86dec06e8106340dd6736
xinxiamu <932852117@qq.com>
2019-01-01 21:57:09 +0800

    修改

---
+++ office-sample/gs-spring-boot-docker/complete/src/main/java/hello/Application.java
@@ -0,0 +1,25 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @Value("${test}")
+    private String testStr;
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World-" + testStr;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
