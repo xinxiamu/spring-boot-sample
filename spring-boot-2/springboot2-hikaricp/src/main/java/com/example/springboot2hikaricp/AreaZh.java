98174bdd827df9dbd3400e410190712ea1d71fee
xinxiamu <932852117@qq.com>
2019-03-29 00:56:08 +0800

    修改

---
+++ spring-boot-2/springboot2-hikaricp/src/main/java/com/example/springboot2hikaricp/AreaZh.java
@@ -0,0 +1,48 @@
+package com.example.springboot2hikaricp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+public class AreaZh  {
+
+    @Column(nullable = false,unique = true,columnDefinition = "varchar(16) comment '行政代码'")
+    private String code;
+
+    @Column(columnDefinition = "decimal(19,8) comment '纬度'")
+    private BigDecimal lat;
+
+    @Column(columnDefinition = "decimal(19,8) comment '经度'")
+    private BigDecimal lng;
+
+    @Column(nullable = false,columnDefinition = "smallint(1) comment '层级'")
+    private Short level;
+
+    @Column(columnDefinition = "varchar(255) comment '组合名称'")
+    private String mergerName;
+
+    @Column(columnDefinition = "varchar(50) comment '名称'")
+    private String name;
+
+    @Column(columnDefinition = "varchar(255) comment '拼音'")
+    private String pinyin;
+
+    @Column(columnDefinition = "varchar(150) comment '简称'")
+    private String shortName;
+
+    @Column(nullable = false,columnDefinition = "varchar(30) comment '电话区号'")
+    private String telCode;
+
+    @Column(nullable = false,columnDefinition = "varchar(50) comment '邮政编码'")
+    private String zipCode;
+
+    @ManyToOne
+    private AreaZh parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<AreaZh> children;
+
+
+
+}
