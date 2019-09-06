340981f758703c0ef660515fdb04786c089f6891
xinxiamu <932852117@qq.com>
2019-09-06 15:57:11 +0800

    学习

---
+++ spring-boot-properties/src/main/java/com/didispace/service/dy/SendMessageAliImpl.java
@@ -0,0 +1,17 @@
+package com.didispace.service.dy;
+
+import com.didispace.service.SendMessage;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class SendMessageAliImpl implements A {
+
+	@Override
+	public SendMessageType getType() {
+		return SendMessageType.ALI;
+	}
+}
