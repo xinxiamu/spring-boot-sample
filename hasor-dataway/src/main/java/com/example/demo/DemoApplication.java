5c57fef5747df32e6b581eaa12b5c8b4a0b332a6
xinxiamu <932852117@qq.com>
2020-05-31 00:09:12 +0800

    好好学习，天天向上

--- hasor-dataway/src/main/java/com/example/demo/DemoApplication.java
+++ hasor-dataway/src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,20 @@
 package com.example.demo;
 
+import net.hasor.spring.boot.EnableHasor;
+import net.hasor.spring.boot.EnableHasorWeb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@EnableHasor
+@EnableHasorWeb
 @SpringBootApplication
 public class DemoApplication {
 
@@ -10,4 +22,20 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Controller
+    @ResponseBody
+    @RequestMapping("/")
+    public class HomeController {
+
+        /**
+         * 输入http://localhost:8080直接跳转到http://localhost:8080/interface-ui/
+         * @param request
+         * @param response
+         * @throws IOException
+         */
+        @GetMapping
+        public void index(HttpServletRequest request, HttpServletResponse response) throws IOException {
+           response.sendRedirect("/interface-ui/");
+        }
+    }
 }
